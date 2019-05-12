b834bd86e2b04b78b9b9e3334bffc96b767f175d
mateuszgujda <43783866+mateuszgujda@users.noreply.github.com>
2019-05-12 18:14:42 +0200

    Added project to Github

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+
+    [SerializeField] float ScreenWidthInUnits = 16f;
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float MouseXinUnits = Input.mousePosition.x / Screen.width * ScreenWidthInUnits;
+        MouseXinUnits = Mathf.Clamp(MouseXinUnits, minX, maxX);
+        Vector2 newPosition = new Vector2(MouseXinUnits,transform.position.y);
+        transform.position = newPosition;
+    }
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+
+    }
+
+    public void LoadPreviousScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if(currentSceneIndex != 0)
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadByName(string sceneToLoad)
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
